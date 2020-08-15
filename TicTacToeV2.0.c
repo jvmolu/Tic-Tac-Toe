a4774cb949b08bc66b55016ea921c56e5162e107
Anmol Singhal <44413960+jvmolu@users.noreply.github.com>
2020-08-15 08:13:35 +0530

    Comments edited

--- TicTacToeV2.0.c
+++ TicTacToeV2.0.c
@@ -1,9 +1,5 @@
 /******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
+TIC TAC TOE
 *******************************************************************************/
 #include<stdio.h>
 #include<stdlib.h>
